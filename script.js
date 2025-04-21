7d8cfa568bb95b1f13980e04998dfd1aa83a1bbc
YusufXojimurodov <xojimurodovyusufjon2@gmail.com>
2025-04-21 19:03:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let orqarangkirgiz = prompt("Orqa fon rangini kirgizing")
+
+let textrangikirgiz = prompt("Textni rangini kirgizing")
+
+document.body.style.backgroundColor = orqarangkirgiz
+
+document.body.style.color = textrangikirgiz
